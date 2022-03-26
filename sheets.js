04322786d13257bb415e9a4c7cd353a94637c885
Matt Kulukundis <matt.fowles@gmail.com>
2022-03-25 23:58:13 -0400

    use short names and the member API

--- sheets.js
+++ sheets.js
@@ -1,31 +1,27 @@
 function myFunction() {
-  const liars = getColumnContent("Senate!B3:B").map(normalizeName);
+  const liarDistricts = getColumnContent("Senate!A3:A");
+  const districtToLiar = getDistrictToLiarShortName();
+  const liarShortNames = liarDistricts.map(district => districtToLiar[district]);
+  console.log(liarShortNames);
   const { labels: labels, range: range } = getBillLabels("Senate!E2:2");
   const columns = labels
     .map(getBill)
-    .map(bill => buildColumn(liars, bill));
+    .map(bill => buildColumn(liarShortNames, bill));
   setColumnContent(range.replace(2, 3).replace(2, ""), columns);
 }
 
-function normalizeName(name) {
-  name = name.toLowerCase();
-  name = name.replaceAll(/[àáâãäå]/g, "a");
-  name = name.replaceAll(/æ/g, "ae");
-  name = name.replaceAll(/ç/g, "c");
-  name = name.replaceAll(/[èéêë]/g, "e");
-  name = name.replaceAll(/[ìíîï]/g, "i");
-  name = name.replaceAll(/ñ/g, "n");
-  name = name.replaceAll(/[òóôõö]/g, "o");
-  name = name.replaceAll(/œ/g, "oe");
-  name = name.replaceAll(/[ùúûü]/g, "u");
-  name = name.replaceAll(/[ýÿ]/g, "y");
-  return name;
-}
-
 function getBill(billLabel) {
-  const url = `https://legislation.nysenate.gov/api/3/bills/2021/${billLabel}?key=${nyGovApiKey}`
+  const url = `https://legislation.nysenate.gov/api/3/bills/2021/${billLabel}?key=${nyGovApiKey}&limit=1000`;
   const response = UrlFetchApp.fetch(url).getContentText();
   return JSON.parse(response).result;
+}
+
+function getDistrictToLiarShortName() {
+  const url = `https://legislation.nysenate.gov/api/3/members/2022/senate?key=${nyGovApiKey}&limit=1000`;
+  const items = JSON.parse(UrlFetchApp.fetch(url).getContentText()).result.items;
+  const districtToName = {};
+  for (item of items) { districtToName[item.districtCode] = item.shortName; }
+  return districtToName;
 }
 
 function getLatestAmendment(bill) {
@@ -36,11 +32,11 @@ function getLatestAmendment(bill) {
 
 function getCoSponsors(bill) {
   const amendment = getLatestAmendment(bill);
-  return amendment.coSponsors.items.map(item => normalizeName(item.fullName));
+  return amendment.coSponsors.items.map(item => item.shortName);
 }
 
 function getSponsor(bill) {
-  return normalizeName(bill.sponsor.member.fullName);
+  return bill.sponsor.member.shortName;
 }
 
 function getColumnContent(col) {
