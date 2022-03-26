b49aff5b8f74e3a2f091e1603dfc47ef58dd23b7
Matt Kulukundis <matt.fowles@gmail.com>
2022-03-25 22:55:40 -0400

    andy hacked some shit

--- sheets.js
+++ sheets.js
@@ -1,13 +1,14 @@
 function myFunction() {
   const liars = getColumnContent("B3:B");
-  const bill = getBill("S6843B");
-  const col = buildColumn(liars, bill);
-  console.log(col);
-  setColumnContent("E3:E", col);
+  const {labels: labels, range: range} = getBillLabels();
+  const columns = labels
+      .map(getBill)
+      .map(bill => buildColumn(liars, bill));
+  setColumnContent(range.replace(2, 3).replace(2, ""), columns);
 }
 
 function getBill(billLabel) {
-  const url = `https://legislation.nysenate.gov/api/3/bills/2021/${billLabel}?key=${apiKey}`
+  const url = `https://legislation.nysenate.gov/api/3/bills/2021/${billLabel}?key=${nyGovApiKey}`
   const response = UrlFetchApp.fetch(url).getContentText();
   return JSON.parse(response).result;
 }
@@ -28,26 +29,38 @@ function getSponsor(bill) {
 }
 
 function getColumnContent(col) {
-  const r = Sheets.Spreadsheets.Values.get(sheedId, col);
-  return r.values.map(x => x[0]);
+  const range = Sheets.Spreadsheets.Values.get(sheetId, col);
+  return range.values.map(x => x[0]);
+}
+
+function getBillLabels() {
+  const range = Sheets.Spreadsheets.Values.get(sheetId, "E2:2");
+  return {
+    labels: range.values[0],
+    range: range.range,
+  };
 }
 
 function buildColumn(liars, bill) {
   const sponsor = getSponsor(bill);
   const coSponsors = getCoSponsors(bill);
-  const result = liars.map(liar => {
+  return liars.map(liar => {
     if (liar == sponsor) return "SPONSOR";
     if (coSponsors.includes(liar)) return "COSPONSOR";
-    return "X";
-
-  })
-  return result;
+    return "";
+  });
 }
 
 function setColumnContent(col, vals) {
-  const updated = Sheets.Spreadsheets.Values.get(sheedId, col);
+  const updated = Sheets.Spreadsheets.Values.get(sheetId, col);
+  console.log(updated.values.length, updated.values[0].length);
+  console.log(vals.length, vals[0].length);
   for (let i = 0; i < vals.length; ++i) {
-    updated.values[i][0] = vals[i];
-  }Sheets.Spreadsheets.Values.update(updated, sheedId, updated.range, {valueInputOption: "RAW"})
+    console.log(updated.values[i].length);
+    for (let j = 0; j < vals[i].length; ++j) {
+      updated.values[j][i] = vals[i][j];
+    }
+  }
+  // console.log(updated.values);
+  Sheets.Spreadsheets.Values.update(updated, sheetId, updated.range, {valueInputOption: "RAW"});
 }
-
